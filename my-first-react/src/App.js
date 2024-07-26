41ab4d7b9648d23f2504c521c32410a868488e53
soumytiwari <tiwarisoumya111@gmail.com>
2024-07-26 11:43:52 +0530

    useEffect

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,20 +1,48 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0)
     return (
         <>
-        <h1>Simple Counter</h1>
-        <p>Count Value: {count}</p >
-
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count - 1)}>Decrement</button>
-        <button onClick={() => setCount(0)}>Reset</button>
+        <DataFetcher/>
         </>
   )
 }
 
 export default App;
 
-// useEffect: when you take data from external database, use me
+function DataFetcher() {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [id, setId] = useState(1)
+
+    // useEffect(() => {}, [])      it takes 2 things: function, dependency array
+    // helping to fetch (at the end, the useEffect will run)
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(response => response.json())
+        .then((data) => {
+            setData(data)
+            setLoading(false)
+        }).catch((error) => {
+            console.error("error in fetching data", error)
+            setLoading(false)
+        })
+    }, [id])
+    // why dependency array?: the useEffect will run only one time, to fetch 2, 3, 4, 5, .. for that only we're using dependency array 
+    // it carries such type of variables, which will be dynamic
+    // normally useEffect will run at the end of the react. But when you use dependency array, it runs everytime the variable is changed. (else, for new dynamic id the data will be loaded, else nopp <try removing id from the dependency array, then see the fetching for other id zz than 1>)
+
+    function fetchNextItem() {
+        setId((prevId) => prevId + 1)
+        setLoading(true)
+    }
+
+    return(
+        <div>
+            <button onClick={fetchNextItem}>Fetch Data</button>
+            <p>{id}</p>
+            {loading ? <p>Loading...</p> : <p>{data.title}</p>}
+        </div>
+    )
+}
---
+++ my-first-react/src/App3.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0)
+    return (
+        <>
+        <h1>Simple Counter</h1>
+        <p>Count Value: {count}</p >
+
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        </>
+  )
+}
+
+export default App;
+
+// useEffect: when you take database from the external system, then use me
