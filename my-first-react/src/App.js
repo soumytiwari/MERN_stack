89dcbd990a4a95236bbda44fed467303f431618c
soumytiwari <tiwarisoumya111@gmail.com>
2024-07-30 23:57:56 +0530

    async, await, and axios

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
     return (
@@ -16,23 +17,36 @@ function DataFetcher() {
     const [loading, setLoading] = useState(true)
     const [id, setId] = useState(1)
 
-    // useEffect(() => {}, [])      it takes 2 things: function, dependency array
-    // helping to fetch (at the end, the useEffect will run)
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(response => response.json())
-        .then((data) => {
-            setData(data)
-            setLoading(false)
-        }).catch((error) => {
-            console.error("error in fetching data", error)
-            setLoading(false)
-        })
-    }, [id])
-    // why dependency array?: the useEffect will run only one time, to fetch 2, 3, 4, 5, .. for that only we're using dependency array 
-    // it carries such type of variables, which will be dynamic
-    // normally useEffect will run at the end of the react. But when you use dependency array, it runs everytime the variable is changed. (else, for new dynamic id the data will be loaded, else nopp <try removing id from the dependency array, then see the fetching for other id zz than 1>)
+    // useEffect(() => {
+    //     fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    //     .then(response => response.json())
+    //     .then((data) => {
+    //         setData(data)
+    //         setLoading(false)
+    //     }).catch((error) => {
+    //         console.error("error in fetching data", error)
+    //         setLoading(false)
+    //     })
+    // }, [id])
 
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                // axios can be used in place of fetch (reduces code)
+                // it itself converts response in response.json() and we get the object
+                const response = await axios(`https://jsonplaceholder.typicode.com/todos/${id}`)
+                // console.log(response.data);
+                setData(response.data)
+                setLoading(false)
+            } catch (error) {
+                console.log('error in fetching data ', error)
+            }
+        }
+        fetchData()
+    }, [id])
+    // whenever the id will change, useEffect will run. Else only at 1st render of the page
+   
     function fetchNextItem() {
         setId((prevId) => prevId + 1)
         setLoading(true)
@@ -46,3 +60,12 @@ function DataFetcher() {
         </div>
     )
 }
+
+// useEfffect will run whenever the page is rendered. useEffect will run when all the UI is rendered.
+// "async and await make promises easier to write"
+
+// async makes a function return a Promise
+// not bothered about what other functions are doing. Just sitting and waiting for its turn to run
+
+// await makes a function wait for a Promise
+// waiting just for the data
---
+++ my-first-react/src/App4.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <>
+        <DataFetcher/>
+        </>
+  )
+}
+
+export default App;
+
+function DataFetcher() {
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [id, setId] = useState(1)
+
+    // useEffect(() => {}, [])      it takes 2 things: function, dependency array
+    // helping to fetch (at the end, the useEffect will run)
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(response => response.json())
+        .then((data) => {
+            setData(data)
+            setLoading(false)
+        }).catch((error) => {
+            console.error("error in fetching data", error)
+            setLoading(false)
+        })
+    }, [id])
+    // why dependency array?: the useEffect will run only one time, to fetch 2, 3, 4, 5, .. for that only we're using dependency array 
+    // it carries such type of variables, which will be dynamic
+    // normally useEffect will run at the end of the react. But when you use dependency array, it runs everytime the variable is changed. (else, for new dynamic id the data will be loaded, else nopp <try removing id from the dependency array, then see the fetching for other id zz than 1>)
+
+    function fetchNextItem() {
+        setId((prevId) => prevId + 1)
+        setLoading(true)
+    }
+
+    return(
+        <div>
+            <button onClick={fetchNextItem}>Fetch Data</button>
+            <p>{id}</p>
+            {loading ? <p>Loading...</p> : <p>{data.title}</p>}
+        </div>
+    )
+}
+
+// convert useEffect part to async, await and then axios (when you use, async await, you don't have to use .then())
